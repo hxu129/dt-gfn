0ace81d3d70683ad51bd26cd985091c0129977e2
胡二狗 <10009383+huergo@user.noreply.gitee.com>
2025-05-07 15:57:58 +0000

    Debug

--- gfn/gflownet/priors/gfn_trees.py
+++ gfn/gflownet/priors/gfn_trees.py
@@ -384,11 +384,13 @@ def compare_trees(tree1, tree2, feature_names, classes_, bounds=None, comp_dist=
             epsilon = 1e-4
             js_div = jensenshannon(probas1, probas2, base=2)
             if np.isnan(js_div): # If jensenshannon still returns NaN (e.g., if a probas was all zeros)
-                probas1 += epsilon
+                probas1 += epsilon 
                 probas2 += epsilon
+                probas1 /= np.sum(probas1)
+                probas2 /= np.sum(probas2)
                 js_div = jensenshannon(probas1, probas2, base=2)
             if np.isnan(js_div):
-                raise ValueError("jensenshannon返回nan")
+                raise ValueError(f"jensenshannon返回nan，probas1是{probas1}, probas2是{probas2}")
             dist_similarity = 1 - js_div
         else:
             # step 1: compare the class label
